023d976c8e1216405a5ebc6470cc19143cb84c5d
Colton stephens <stephens.colton@icloud.com>
2020-02-26 22:39:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,7 +13,15 @@
 #     }
 #   }
 # }
-# 
+# PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
 # PORTION_2 = {
 #   label: "Order"
 # }
